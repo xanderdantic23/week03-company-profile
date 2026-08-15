d3e12f626389750bb362440e28676f20b3e0ba6f
Xander Ezekiel Dantic <xanderdantic23@gmail.com>
2026-08-15 13:26:44 +0800

    feat: create Laravel project

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompanyController;
+
+Route::get('/', [CompanyController::class, 'home'])->name('home');
+Route::get('/about', [CompanyController::class, 'about'])->name('about');
+Route::get('/services', [CompanyController::class, 'services'])->name('services');
+Route::get('/contact', [CompanyController::class, 'contact'])->name('contact');
